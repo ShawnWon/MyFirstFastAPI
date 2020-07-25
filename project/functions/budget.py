d628c97df0d21aafc0e005d19f3ae939dbb5c42c
ShawnWon <47803938+ShawnWon@users.noreply.github.com>
2020-07-25 19:52:52 +0800

    biz logic complete without considering currency code

--- project/functions/budget.py
+++ project/functions/budget.py
@@ -1,12 +1,14 @@
 from ..models.budgetmodel import BudgetReq,BudgetItem
 import pymongo
-import datetime
+from datetime import datetime
 import uuid
 import json
+import calendar
 
 client=pymongo.MongoClient("mongodb+srv://shawn:shawn@cluster0.uebyo.mongodb.net/plannerbee?retryWrites=true&w=majority")
 db = client["plannerbee"]
 col = db["budgets_users"]
+transcol = db["transactions_users"]
 
 
 def initiateBudget():
@@ -53,37 +55,53 @@ def initiateBudget():
 
 def createBudget(budgetreq:BudgetReq):
     userbudget = col.find_one({"_id": budgetreq.pb_identifier})
-    budgetitem = BudgetItem()
-    budgetitem.category = budgetreq.category
-    budgetitem.type = budgetreq.budget_type
-    budgetitem.created_at = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
-    budgetitem.period = "a"
-    budgetitem.budget_advise = "a"
-    budgetitem.budget_period = "a"
-    budgetitem.local_currency_amount_allowed = 1
-    budgetitem.local_currency_period_spent = 1
-    budgetitem.local_currency_period_remaining = 1
-    budgetitem.budget_notes = "a"
     uuidcode = str(uuid.uuid1())
+    now=datetime.now()
+    year=now.strftime("%Y")
+    mon=now.strftime("%m")
+    calmon = now.strftime("%b")
+    if budgetreq.budget_type=="Annual":
+        bperiod= "01 Jan "+now.strftime("%Y")+" - 31 Dec "+now.strftime("%Y")
+        start = datetime.strptime(year+"/1/1",'%Y/%m/%d')
+        end = datetime.strptime(year+"/12/31",'%Y/%m/%d')
+    elif budgetreq.budget_type=="Monthly": 
+        bperiod= "01 "+calmon+" "+year+" - "+str(calendar.monthrange(int(year), int(mon))[1])+" "+calmon+" "+year
+        start=datetime.strptime(year+"/"+mon+"/1",'%Y/%m/%d')
+        end = datetime.strptime(year+"/"+mon+"/"+str(calendar.monthrange(int(year), int(mon))[1]),'%Y/%m/%d')
+    
+    usertrans = transcol.find_one({"_id": budgetreq.pb_identifier})["transactions"]
+    periodspent=0
+    for key in usertrans:
+        dateobj= datetime.strptime(usertrans[key]["transacted_at"],'%Y-%m-%dT%H:%M:%SZ')
+        if dateobj>start and usertrans[key]["category"]==budgetreq.category  :
+            periodspent+=usertrans[key]["base_currency_amount"]
 
+    
+    if (budgetreq.amount+round(periodspent,2))<0 :
+        status="status_above_100"
+    else:
+        status="status_below_100"
+
+    advise = round((budgetreq.amount+round(periodspent,2))/(end-now).days,2)
+    
     bitem ={
             "category": budgetreq.category,
             "type": budgetreq.budget_type,
-            "period": "072020",
-            "budget_advise": "SGD 0/day",
-            "budget_period": "01 Jan 2020 - 31 Dec 2020",
-            "local_currency_amount_allowed": 100,
-            "local_currency_period_spent": -860.82,
-            "local_currency_period_remaining": -760.82,
-            "created_at": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
-            "budget_notes": "status_above_100"
+            "period": now.strftime("%m%Y"),
+            "budget_advise": "SGD "+str(advise)+"/day",
+            "budget_period": bperiod,
+            "local_currency_amount_allowed": budgetreq.amount,
+            "local_currency_period_spent": round(periodspent,2),
+            "local_currency_period_remaining": round(budgetreq.amount+periodspent,2),
+            "created_at": now.strftime("%Y-%m-%dT%H:%M:%SZ"),
+            "budget_notes": status
         }
 
     userbudget["budgets"][uuidcode]=bitem
 
     myquery={"_id": budgetreq.pb_identifier}
     newvalue= {"$set":{"budgets":userbudget["budgets"]}}
-    col.update_one(myquery,newvalue)
-    print(type(userbudget["budgets"]))
-    print(type({}))
-    return userbudget["budgets"]
+    result=col.update_one(myquery,newvalue)
+
+    
+    return result.modified_count
--- project/routers/router.py
+++ project/routers/router.py
@@ -10,7 +10,11 @@ router = APIRouter()
 @router.post("/api/addbudget")
 async def add_budget(budgetreq:BudgetReq):
     x = createBudget(budgetreq)
-    return x
+    if x>0:
+        result={"status":"success","message":"successfully created budget"}
+    else:
+        result={"status":"error","message":"failed to create budget"}
+    return result
 
 @router.post("/api/initiatetransaction")
 async def initiatetransaction():
